3c9c4f02dd83e6b241aed1de7ed9f20ba0160bd7
idonans <idonans@126.com>
2017-03-30 15:38:55 +0800

    add LeakCanary, build_config.gradle, up boot to 0.7.1

--- sample/src/main/java/com/sample/share/SampleApplication.java
+++ sample/src/main/java/com/sample/share/SampleApplication.java
@@ -2,6 +2,8 @@ package com.sample.share;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 /**
  * Created by idonans on 2017/2/3.
  */
@@ -11,6 +13,11 @@ public class SampleApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        if (!LeakCanary.isInAnalyzerProcess(this)) {
+            LeakCanary.install(this);
+        }
+
         AppInit.init(this);
     }
 
